452b50e9284617bd761a9b3a7c7daf6a2afb3994
karenli95 <kjl32@duke.edu>
2015-11-29 15:06:07 -0500

    Updated Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,57 +1,56 @@
 import java.sql.*;
 import java.util.HashMap;
-import java.util.ArrayList;
 import java.util.Map;
 
 import static spark.Spark.*;
 import spark.template.freemarker.FreeMarkerEngine;
 import spark.ModelAndView;
-import static spark.Spark.get;
 
 import com.heroku.sdk.jdbc.DatabaseUrl;
 
 public class Main {
+	private static String myJSon = "";
 
-  public static void main(String[] args) {
+	public static void main(String[] args) {
 
-    port(Integer.valueOf(System.getenv("PORT")));
-    staticFileLocation("/public");
+		port(Integer.valueOf(System.getenv("PORT")));
+		staticFileLocation("/public");
+		
+		get("/", (request, response) -> {
+			if(myJSon == ""){
+				ParseAddresses parser = new ParseAddresses();
+				myJSon = parser.readLargeFile2("src/main/resources/data/Addresses_small_columns6.csv");
+			}
+			
+			Map<String, Object> attributes = new HashMap<>();
 
-    get("/hello", (req, res) -> "Hello World");
+			return new ModelAndView(attributes, "bounds.ftl");
+		}, new FreeMarkerEngine());
 
-    get("/", (request, response) -> {
-            Map<String, Object> attributes = new HashMap<>();
-            attributes.put("message", "Hello World!");
 
-            return new ModelAndView(attributes, "index.ftl");
-        }, new FreeMarkerEngine());
+		post("/boundsanswers", (request, response) -> {
+			String minLat= "";
+			String maxLat = "";
+			String minLon = "";
+			String maxLon = "";
+			try {
+				minLat= request.queryParams("minLat") != null ? request.queryParams("minLat") : "";
+				maxLat= request.queryParams("maxLat") != null ? request.queryParams("maxLat") : "";
+				minLon= request.queryParams("minLon") != null ? request.queryParams("minLon") : "";
+				maxLon= request.queryParams("maxLon") != null ? request.queryParams("maxLon") : "";
+			} catch (Exception e) {
 
-    get("/db", (req, res) -> {
-      Connection connection = null;
-      Map<String, Object> attributes = new HashMap<>();
-      try {
-        connection = DatabaseUrl.extract().getConnection();
+			}
 
-        Statement stmt = connection.createStatement();
-        stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+			Bounds bounds = new Bounds(myJSon, minLat, maxLat, minLon, maxLon);
+			String answer = bounds.parseJson();
 
-        ArrayList<String> output = new ArrayList<String>();
-        while (rs.next()) {
-          output.add( "Read from DB: " + rs.getTimestamp("tick"));
-        }
+			Map<String, Object> attributes = new HashMap<>();
+			attributes.put("jsonAddresses", answer);
 
-        attributes.put("results", output);
-        return new ModelAndView(attributes, "db.ftl");
-      } catch (Exception e) {
-        attributes.put("message", "There was an error: " + e);
-        return new ModelAndView(attributes, "error.ftl");
-      } finally {
-        if (connection != null) try{connection.close();} catch(SQLException e){}
-      }
-    }, new FreeMarkerEngine());
+			return new ModelAndView(attributes, "heatmap.ftl");
+		}, new FreeMarkerEngine());
 
-  }
+	}
 
 }
